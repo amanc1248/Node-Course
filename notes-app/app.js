095d125eb1aea0cbb86e1c01dd935065b357b7d2
amanc1248 <amanc1248@gmail.com>
2020-09-21 11:01:01 +0545

    First App, Note taking app

---
+++ notes-app/app.js
@@ -0,0 +1,22 @@
+const validator = require("validator")
+const yourNotes = require("./notes.js")
+const myNotes = yourNotes();
+console.log(myNotes);
+console.log(validator.isURL('http://gmail.com'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require("./util.js");
+// const  name = "Aman"
+// console.log(name);
